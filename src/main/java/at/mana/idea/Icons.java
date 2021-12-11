63439adb5d62f07d3236d1f8fbb2ea7fe82f0fb9
Andreas Schuler <andreas.schuler@fh-hagenberg.at>
2021-12-12 00:55:15 +0100

    #16 additional icons

--- src/main/java/at/mana/idea/Icons.java
+++ src/main/java/at/mana/idea/Icons.java
@@ -9,5 +9,6 @@ public class Icons {
     public static final Icon LOGO =  IconLoader.getIcon( "/icons/logo.svg", Icons.class );
     public static final Icon LOGO_GUTTER =  IconLoader.getIcon( "/icons/logo_13.svg", Icons.class );
     public static final Icon LOGO_EMPTY_BG_GUTTER =  IconLoader.getIcon( "/icons/no_background_13.svg", Icons.class );
+    public static final Icon LOGO_EMPTY_BW =  IconLoader.getIcon( "/icons/no_background_bw.svg", Icons.class );
 
 }

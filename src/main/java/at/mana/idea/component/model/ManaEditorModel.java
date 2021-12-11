71861e2671c4b11a1db9c2dd4026f0777fa3be88
Andreas Schuler <andreas.schuler@fh-hagenberg.at>
2021-12-11 11:06:36 +0100

    #13 updated classes - added license info doc

--- src/main/java/at/mana/idea/component/model/ManaEditorModel.java
+++ src/main/java/at/mana/idea/component/model/ManaEditorModel.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2020 the original author or authors.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 package at.mana.idea.component.model;
 
 import at.mana.idea.domain.ClassEnergyStatistics;
